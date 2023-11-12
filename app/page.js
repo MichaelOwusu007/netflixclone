55ab8200e40f6b97876c8fdf54b37c27d020babc
michael Owusu <nanayawwiafe007@gmail.com>
2023-11-12 14:31:00 +0000

    Netflix

--- app/page.js
+++ app/page.js
@@ -8,8 +8,8 @@ function Homepage() {
         <nav className='flex items-center  justify-between py-2 px-0'>
           <img src="/images/logo.png" className='logo lg:w-[150px] w-[90px] md:w-[120px] cursor-pointer  '/>
           <div>
-            <button className='border rounded py-2 text-[12px] px-2 bg-[#db0001] lg:inline-flex items-center bg-transparent hidden '>English <img src="/images/down-icon.png" className='ml-[10px] w-[10px] '/></button>
-            <button className='border py-2 px-5 text-[12px] bg-[#db0001] ml-4 rounded '>Sign In</button>
+            <button className='border rounded sm:py-2 py-1 text-[12px] sm:px-5 px-2 bg-[#db0001] inline-flex items-center bg-transparent  '>English <img src="/images/down-icon.png" className='ml-[10px] w-[10px] '/></button>
+            <button className='border py-1 px-2 sm:py-2 sm:px-5 text-[12px] bg-[#db0001] ml-4 rounded '>Sign In</button>
           </div>
         </nav>
         <div className='header-content absolute top-[50%] left-[50%] text-center mt-[100px] '>
@@ -18,7 +18,7 @@ function Homepage() {
           <p>Readyto watch? Enter your email to create or resatert your membership today.</p>
           <form className='email-signup bg-white flex rounded overflow-hidden items-center mt-[30px] text-black '>
             <input type="email" placeholder="Email address" required className='flex-1 outline-0 border-0 ml-[20px] ' />
-            <button type='submit' className='border-0  bg-[#db0000] outline-0 lg:py-[15px] py-[5px] lg:px-[30px] px-[20px] text-[16px] text-white cursor-pointer  ' >Get Started</button>
+            <button type='submit' className='border-0  bg-[#db0000] outline-0 lg:py-[15px] sm:py-[8px] lg:px-[30px] px-[20px] text-[16px] text-white cursor-pointer  ' >Get Started</button>
           </form>
         </div>
       </header>
@@ -128,12 +128,45 @@ function Homepage() {
           </li>
         </ul>
         <small className='font-bold text-[15px]  lg:text-lg '>Ready to watch? enter your email to create or start your membership</small>
-         <form className='email-signup bg-white flex rounded overflow-hidden items-center mt-[30px] text-black lg:max-w-[600px] mx-auto mb-[60px] '>
+         <form className='email-signup bg-white flex rounded overflow-hidden items-center w-full mt-[30px] text-black max-w-[600px] mx-auto mb-[60px] '>
             <input type="email" placeholder="Email address" required className='flex-1 outline-0 border-0 ml-[20px] ' />
-            <button type='submit' className='border-0  bg-[#db0000] outline-0 lg:py-[15px] py-[5px] lg:px-[30px] px-[20px] m-auto text-[16px] text-white cursor-pointer  ' >Get Started</button>
+            <button type='submit' className='border-0  bg-[#db0000] outline-0 lg:py-[15px] py-[8px] lg:px-[30px] px-[20px] m-auto text-[16px] text-white cursor-pointer ' >Get Started</button>
           </form>
       </div>
+
+      <div className='footer py-[50px] lg:px-[15%] px-[15%] sm:px-[5%]  pb-[20px]  border-t-4 border-[#777] text-[#777] md:px-[10%] text-[16px] leading-8 w-full '>
+        <h2>Questions? call 000-000-000-000</h2>
       
+      <div className=' flex flex-col sm:flex-row py-[50px] '>
+        <div className='col flex flex-col flex-grow flex-[25%]  '>
+          <a href='#'>FAQ</a>
+          <a href='#'>Investor Relation</a>
+          <a href='#'>Privacy</a>
+          <a href='#'>Speed Test</a>
+        </div>
+          <div className='col flex flex-col  flex-grow flex-[25%] '>
+          <a href='#'>FAQ</a>
+          <a href='#'>Investor Relation</a>
+          <a href='#'>Privacy</a>
+          <a href='#'>Speed Test</a>
+        </div>
+          <div className='col flex flex-col  flex-grow flex-[25%] '>
+          <a href='#'>FAQ</a>
+          <a href='#'>Investor Relation</a>
+          <a href='#'>Privacy</a>
+          <a href='#'>Speed Test</a>
+        </div>
+          <div className='col flex flex-col  flex-grow flex-[25%] '>
+          <a href='#'>FAQ</a>
+          <a href='#'>Investor Relation</a>
+          <a href='#'>Privacy</a>
+          <a href='#'>Speed Test</a>
+        </div>
+        </div>
+        <button className='border rounded py-2 text-[12px] px-5  text-white inline-flex  items-center bg-transparent '>English <img src="/images/down-icon.png" className='ml-[10px] w-[10px] ' /></button>
+        <p className='copyright-text text-[14px] mt-[20px] mb-[10px] '>Netflix Right On Your Screen</p>
+      </div>
+      <div></div>
     </div>
   )
 }
